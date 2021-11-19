b0d145dda332ae19b094b0a6e7780422580665cd
Fatih Furkan Aydemir <furkanaydemir6@gmail.com>
2021-11-19 23:56:10 +0300

    Visualizing method changed and merge sort added

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 'use strict';
 
-// class Bar {
-//   _x;
-//   _y;
-//   _width;
-//   _height;
-
-//   constructor(x, y, width, height) {
-//     this._x = x;
-//     this._y = y;
-//     this._width = width;
-//     this._height = height;
-//   }
-// }
-
+const speed = 10;
 const arraySize = 100;
 let barValues = [];
+let arrayToVisualize = [];
+let states = [];
 
 for (let i = 0; i < arraySize; i++) {
   barValues.push(Math.random() * 600 + 10);
 }
+
+arrayToVisualize = barValues.slice();
 
 const canvas = document.querySelector('#canvas');
 const canvasSection = document.querySelector('.canvas-section');
@@ -30,7 +21,10 @@ window.addEventListener('resize', ResizeCanvas);
 
 startBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  insertionSort(barValues);
+  //insertionSort(barValues);
+  mergeSort(barValues);
+
+  Animate();
 });
 
 randomizeBtn.addEventListener('click', function (e) {
@@ -38,8 +32,10 @@ randomizeBtn.addEventListener('click', function (e) {
 
   barValues = [];
   for (let i = 0; i < arraySize; i++) {
-    barValues.push(Math.random() * canvas.clientHeight + 10);
+    barValues.push(Math.random() * (canvas.clientHeight - 10) + 10);
   }
+
+  arrayToVisualize = barValues.slice();
 
   Draw();
 });
@@ -53,7 +49,23 @@ function ResizeCanvas() {
 
 ResizeCanvas();
 
-function Draw(array = barValues, highlighted = -1) {
+function Animate() {
+  states.forEach(function (state, i) {
+    setTimeout(function () {
+      Draw(arrayToVisualize, state.index);
+      if (state.type === 'set') {
+        arrayToVisualize[state.index] = state.value;
+      }
+    }, speed * i);
+  });
+
+  setTimeout(function () {
+    Draw();
+    states = [];
+  }, speed * states.length);
+}
+
+function Draw(array = arrayToVisualize, highlighted = -1) {
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
 
@@ -77,19 +89,59 @@ function Draw(array = barValues, highlighted = -1) {
 
 function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
-    setTimeout(function () {
-      const key = array[i];
-      let j = i - 1;
+    const key = array[i];
+    let j = i - 1;
 
-      while (j >= 0 && array[j] > key) {
-        array[j + 1] = array[j];
-        j--;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      states.push({ type: 'set', index: j + 1, value: array[j] });
+      j--;
+    }
 
-        Draw(array, j + 1);
-      }
+    array[j + 1] = key;
+    states.push({ type: 'set', index: j + 1, value: key });
+  }
+}
 
-      array[j + 1] = key;
-      if (i === array.length - 1) Draw();
-    }, 50 * i);
+function mergeSort(array, p = 0, r = array.length - 1) {
+  if (p < r) {
+    const q = Math.floor((p + r) / 2);
+    mergeSort(array, p, q);
+    mergeSort(array, q + 1, r);
+    merge(array, p, q, r);
+  }
+}
+
+function merge(array, p, q, r) {
+  const n1 = q - p + 1;
+  const n2 = r - q;
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < n1; i++) {
+    left.push(array[p + i]);
+  }
+
+  for (let i = 0; i < n2; i++) {
+    right.push(array[q + i + 1]);
+  }
+
+  left.push(Infinity);
+  right.push(Infinity);
+
+  let k = 0,
+    m = 0;
+
+  for (let i = p; i <= r; i++) {
+    if (left[k] <= right[m]) {
+      array[i] = left[k];
+      states.push({ type: 'set', index: i, value: left[k] });
+      k++;
+    } else {
+      array[i] = right[m];
+      states.push({ type: 'set', index: i, value: right[m] });
+      m++;
+    }
   }
 }
